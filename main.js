8cc3128f7ec3b9e51bffa147c77b9f19ce9d0229
andrelopestec <134522308+andrelopestec@users.noreply.github.com>
2023-10-23 09:55:26 -0300

    corrigindo erro de som

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom();
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
